118748f26bda37d03dc20cf9afa27ec96bcbb724
Nikolay Ignatyev <noilgt@gmail.com>
2015-04-02 18:58:19 +0300

    Added sample view without templates

--- app/views.py
+++ app/views.py
@@ -3,4 +3,15 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return '<h1>Hello my Flask world!</h1>'
+    user = {'userName': 'Antony Sk'} # Some user
+    return '''
+<html>
+    <head>
+	<title>Flask tutorial Task 02</title>
+    </head>
+    <body>
+	<hr>
+	<h1 align = "center"> Hello, my dear friend!''' + user['userName'] + '''</h1>
+    </body>
+</html>
+'''
